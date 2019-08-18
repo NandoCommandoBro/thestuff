fac62eefb8a35e5465239643c1e6f647ef7289ac
NandoCommandoBro <53320633+NandoCommandoBro@users.noreply.github.com>
2019-08-17 20:47:59 -0500

    Add files via upload

---
+++ includes/header.php
@@ -0,0 +1,15 @@
+<nav class="navbar navbar-expand-md transparent fixed-top navbar-light ">
+    <div class="container">
+        <a href="#" class="navbar-brand"><img width=40 height=40 src="images/transparentMinecraft.png" alt="IlikeMinecraftbro"></a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse ml-auto navbar-collapse" id="navbarNavAltMarkup">
+            <div class="navbar-nav text-right ml-auto">
+                <a href="#" class="nav-item nav-link">Home</a>
+                <a href="#" class="nav-item nav-link">About</a>
+                <a href="#" class="nav-item nav-link">Contact</a>
+            </div>
+        </div>
+    </div>
+</nav>
